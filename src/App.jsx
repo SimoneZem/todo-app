3df988a8881e4a0267417e2b6bdf60ca40d0b900
Simone Trita <simonetrita@gmail.com>
2022-04-20 18:51:16 +0200

    feat(add formComponent): add form as component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>ToDo App</h1>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {
-  return <div>Form</div>;
-}
+export const Form = () => {
+  const [input, setInput] = useState("");
+  return (
+    <form className="todo-form">
+      <input
+        type="text"
+        placeholder="Add an activity"
+        value={input}
+        name="text"
+        className="todo-input"
+      />
+      <button className="todo-button">Add Activity</button>
+    </form>
+  );
+};
 
 export default Form;
