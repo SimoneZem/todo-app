4ce4db22bc52e6e688c07e3ecf740a964b53de22
Simone Trita <simonetrita@gmail.com>
2022-04-21 13:07:18 +0200

    feat(add handleButton): add handleButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+/*
+Obiettivi per prossima settimana: 
+devi fare una todo list che abbia i seguenti requisiti 
+
+1) deve avere un form che
+ - permetta di inserire il nome di un task e la descrizione. 
+
+2) se premi invia e uno dei due campi del form e' vuoto deve comparire un messaggio d'errore.  
+
+se i campi hanno entrambi un item alla pressione del bottone "add item" deve comparire un item in una lista. 
+
+Questo item dovra' contenere le seguenti info: 
+
+1) Nome del task 
+2) descrizione del task ( informazioni inseriti precedentemente nel form ) 
+3) una dropdown che permetta il cambio di stato del task. 
+
+Gli stati saranno 3: 
+
+1) done
+2) not done 
+bonus point
+ ---------
+3) planned for 
+
+Se premo planned for dovra' comparire un altro form all'interno del task relativo che mi fara' inserire la data di completamento. 
+--------------  Fine del bonus point, 
+
+da qui sotto in poi sono di nuovo punti FONDAMENTALI da fare 
+
+1) ogni item dovra' avere la possibilit' di essere cancellato.  
+2) ogni item dovra' avere la possiblita' di essere modificato. 
+
+Il tutto dovra' avere un aspetto umano e non una merda.
+
+al cambio dello stato il ticket dovra' cambiare colore dello sfondo.
+
+done verde, not done rosso, planned for giallo.
+*/
+
+import React from "react";
 import "./App.css";
 import Form from "./components/Form";
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
 
-export const Form = () => {
+export const Form = (props) => {
   const [input, setInput] = useState("");
+
+  const handleChange = (y) => {
+    setInput(y.target.value);
+  };
+
+  /*in questo momento, quando si clicca su add activities la pagina refresha. Non è ciò che vogliamo, 
+andremo quindi a creare la funzione che verrò lanciata nel momento in cui verrà premuto il tasto. 
+La chiameremo handelButton ( 10.59  preventDefault)
+*/
+  const handleButton = (x) => {
+    x.preventDefault();
+    /*
+A questo punto aggiungiamo una props al nostro componente perchè andremo ad aggiungere in sehuito altri dati 
+negli altri componenti e quindi questo ci consentirà sostanzialmente di accedervi  
+  */
+
+    /* props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    });
+  };*/
+
+    setInput("");
+  };
+
   return (
-    <form className="todo-form">
+    <form className="todo-form" onSubmit={handleButton}>
       <input
         type="text"
         placeholder="Add an activity"
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
       <button className="todo-button">Add Activity</button>
     </form>
@@ -17,3 +43,5 @@ export const Form = () => {
 };
 
 export default Form;
+
+//14.40
